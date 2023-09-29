365c8bf22cb0f279e1bc01217a3cf9160dbf31d4
dgagan2 <andgag@duck.com>
2023-09-29 17:02:26 -0500

    Cambio manejo de errores de los roles

--- API/controllers/user/role.js
+++ API/controllers/user/role.js
@@ -4,17 +4,23 @@ const {lettersToLowercase} = require("../../utils/lowercase")
 
 const addRole=asyncHandler(async (req, res)=>{
     try {
-  const name= req.body.name
-    const newRole=await Role.create({name:lettersToLowercase(name)})
-        res.status(201).json(newRole)
-} catch (error) {
-  res.status(500).json({message:'No se creo el role', error})
-}
+        const name= req.body.name
+        if(!name){
+           return res.status(500).json({message:'Nombre del role vacio'})
+        }
+        const newRole=await Role.create({name:lettersToLowercase(name)})
+            res.status(201).json(newRole)
+    } catch (error) {
+        res.status(500).json({message:'No se creo el role', error})
+    }
   
 })
 
 const updateRole=async (req, res)=>{
     const {id, name}= req.body
+    if(!id || !name){
+        res.status(500).json({message:'Id o nombre vacios'})
+    }
     try {
         const newRole=await Role.findByIdAndUpdate(id, {name:lettersToLowercase(name)}, {new:true})
         res.status(200).json(newRole)
@@ -26,10 +32,13 @@ const updateRole=async (req, res)=>{
 
 const deleteRole=asyncHandler(async (req, res)=>{
     const {id}= req.params
-    const Delete=await Role.findByIdAndDelete(id)
-    if(Delete){
+    if(!id){
+        res.status(500).json({message:'Id vacio'})
+    }
+    try {
+        const Delete=await Role.findByIdAndDelete(id)
         res.status(200).json(Delete)
-    }else{
+    } catch (error) {
         res.status(500).json({message:'Role no existe'})
     }
     
