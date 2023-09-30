f2d8ea328c4e7af61ce55b24d33463b567c9b7fa
Deibi <114509458+dgagan2@users.noreply.github.com>
2023-09-30 14:00:52 -0500

    Update movie.js

--- API/controllers/movie/movie.js
+++ API/controllers/movie/movie.js
@@ -8,41 +8,34 @@ const updateMovie=asyncHandler(async (req, res)=>{
             releaseDate, director, actors, originalTitle,
             labels, premiere }=req.body
         const {posterPath, postBackground}=req.files
-        try {
-                if(!id || !title || !overview || !genre || !language|| !releaseDate){
-                      return  res.status(500).json({message:'Campos obligatorios vacios'})
-                }
-            if(posterPath && posterPath.length>0){
-                    var {downloadURL}=await uploadFile(posterPath[0], 'poster')
-            }else{
-                   return res.status(500).json({message:'Imagen de poster vacia'})
-            }
-            if(postBackground && postBackground.length>0){
-                   var background=await uploadFile(postBackground[0], 'background')
-            }else{
-                   return res.status(500).json({message:'Imagen de fondo vacia'})
-            }
-                const newMovie=await Movie.findByIdAndUpdate(id, {
-                    title: primeraLetraMayuscula(title),
-                    overview,
-                    genre,
-                    language,
-                    movieDuration,
-                    releaseDate,
-                    director,
-                    actors,
-                    originalTitle,
-                    adult,
-                    labels,
-                    premiere,
-                    posterPath:downloadURL,
-                    postBackground:background.downloadURL
-                })
-                res.status(200).json(newMovie)
-        } catch (error) {
-                  res.status(400).json({message:'No se pudo actualizar la pelicula', error})
+        if(posterPath && posterPath.length>0){
+            var {downloadURL}=await uploadFile(posterPath[0], 'poster')
         }
-}     
+        if(postBackground && postBackground.length>0){
+            var background=await uploadFile(postBackground[0], 'background')
+        }
+        const newMovie=await Movie.findByIdAndUpdate(id, {
+            title: primeraLetraMayuscula(title),
+            overview,
+            genre,
+            language,
+            movieDuration,
+            releaseDate,
+            director,
+            actors,
+            originalTitle,
+            adult,
+            labels,
+            premiere,
+            posterPath:downloadURL,
+            postBackground:background.downloadURL
+        })
+        if(newMovie){
+            res.status(200).json(newMovie)
+        }else{
+            res.status(400).json({message:'No se pudo actualizar la pelicula'})
+        }   
+    }
 )
 
 const deleteMovie=async (req, res)=>{
